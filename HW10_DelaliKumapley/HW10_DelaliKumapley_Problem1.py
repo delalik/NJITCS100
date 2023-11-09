2b9bdd0ed4157bf3208ce4f6d9069b1ccd061047
delalik <dak24@njit.edu>
2023-11-09 16:51:15 -0500

    Finalized HW9 Problem 2 Solution

---
+++ HW10_DelaliKumapley/HW10_DelaliKumapley_Problem1.py
@@ -0,0 +1,15 @@
+#Delali Kumapley
+#CS 100 H11
+#Homework 10
+#Due Date: November 16th
+
+'''
+Problem 1
+Write a function named initialLetterCount that takes one parameter, wordList — a list of words. Create and
+return a dictionary in which each initial letter of a word in wordList is a key and the corresponding value
+is the number of words in wordList that begin with that letter. The keys in the dictionary should be case-sensitive, which means 'a' and 'A' are two different keys.
+For example, the following is correct output:
+horton = ['I', 'say', 'what', 'I', 'mean', 'and', 'I', 'mean', 'what', 'I', 'say']
+print(initialLetterCount(horton))
+{'I': 4, 's': 2, 'w': 2, 'm': 2, 'a': 1}
+'''
